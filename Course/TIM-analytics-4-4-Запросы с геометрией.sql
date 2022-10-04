58b3ed59d46beab05f24b9e059930ed6677afcb7
Georg Grebenyuk <55656376+GeorgGrebenyuk@users.noreply.github.com>
2022-10-04 20:02:59 +0300

    Delete TIM-analytics-4-4-Запросы с геометрией.sql

--- Course/TIM-analytics-4-4-Запросы с геометрией.sql
+++
@@ -1,6 +0,0 @@
---after
-alter table pipes add column is_under_surface BOOLEAN;
-
-update pipes set is_under_surface = st_intersects(surfaces.geom, geom);
-SELECT * FROM public.pipes
-ORDER BY id ASC LIMIT 100;
